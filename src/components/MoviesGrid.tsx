ccfc1683a068d571672a216f4a0ec795d4b9c9e1
mmutuku <mmutuku@safaricom.co.ke>
2024-09-27 16:08:06 +0300

    Fetch movies from json file and render in MovieGrid

--- src/components/MoviesGrid.tsx
+++ src/components/MoviesGrid.tsx
@@ -1,13 +1,35 @@
 import "../styles.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, Key } from "react";
 
 export default function MoviesGrid() {
-  const [movies, setMovies]: any[] = useState([]);
+  const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    const m = ["aaa", "bbb"];
-    setMovies(m);
+    fetch("movies.json")
+      .then((response) => response.json())
+      .then((data) => setMovies(data));
   }, []); // when the component renders first
 
-  return <div>{movies.length}</div>;
+  return (
+    <div className="movies-grid">
+      {movies.map(
+        (movie: {
+          id: Key | null | undefined;
+          image: any;
+          title: string | undefined;
+          genre: string | undefined;
+          rating: string | undefined;
+        }) => (
+          <div key={movie.id} className="movie-card">
+            <img src={`images/${movie.image}`} alt={movie.title} />
+            <div className="movie-card-info">
+              <h3 className="movie-card-title">{movie.title}</h3>
+              <p className="movie-card-genre">{movie.genre}</p>
+              <p className="movie-card-rating">{movie.rating}</p>
+            </div>
+          </div>
+        )
+      )}
+    </div>
+  );
 }
