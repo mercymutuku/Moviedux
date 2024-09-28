9b7de0ec1b33ee2ed3e16f18627fc7d837312b17
mmutuku <mmutuku@safaricom.co.ke>
2024-09-28 13:28:53 +0300

    separate MovieCard from MovieGrid; handle image src errors

--- src/components/MoviesGrid.tsx
+++ src/components/MoviesGrid.tsx
@@ -1,5 +1,6 @@
 import "../styles.css";
-import { useState, useEffect, Key } from "react";
+import { useState, useEffect } from "react";
+import MovieCard from "./MovieCard";
 
 export default function MoviesGrid() {
   const [movies, setMovies] = useState([]);
@@ -12,24 +13,9 @@ export default function MoviesGrid() {
 
   return (
     <div className="movies-grid">
-      {movies.map(
-        (movie: {
-          id: Key | null | undefined;
-          image: any;
-          title: string | undefined;
-          genre: string | undefined;
-          rating: string | undefined;
-        }) => (
-          <div key={movie.id} className="movie-card">
-            <img src={`images/${movie.image}`} alt={movie.title} />
-            <div className="movie-card-info">
-              <h3 className="movie-card-title">{movie.title}</h3>
-              <p className="movie-card-genre">{movie.genre}</p>
-              <p className="movie-card-rating">{movie.rating}</p>
-            </div>
-          </div>
-        )
-      )}
+      {movies.map((movie) => (
+        <MovieCard movie={movie} key={movie.id}></MovieCard>
+      ))}
     </div>
   );
 }
